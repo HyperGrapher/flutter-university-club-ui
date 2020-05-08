f17bf6686d5f06adb170964a48db17bf963ba751
Burak Mestan <burakmestan@gmail.com>
2020-05-08 11:37:56 +0300

    cleaning before first delivery

--- lib/pages/add.dart
+++ lib/pages/add.dart
@@ -1,41 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
-import 'package:universityapp/services/database.dart';
 
 class AddPage extends StatelessWidget {
-  _sendData() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    int uid = prefs.getInt('uid');
-    print('- - - - > Firestore UID: $uid');
-
-    await DatabaseService().updateData(
-        uid,
-        "new host_university",
-        "new host_department",
-        "new host_major",
-        "new_host_lecture_id_5050",
-        "new host_lecture_name",
-        "new eras_university",
-        "new eras_department",
-        "new eras_major",
-        "new eras_lecture_id",
-        "new eras_lecture_name");
-  }
-
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child: RaisedButton(
-          color: Colors.red,
-          textColor: Colors.white,
-          padding: EdgeInsets.fromLTRB(34.0, 12.0, 34.0, 12.0),
-          onPressed: () {
-            _sendData();
-          },
-          child: Text("Send"),
-        ),
+        child: Text('Add Lecture Page'),
       ),
     );
   }
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -2,52 +2,11 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class DatabaseService {
-  final CollectionReference lectureCollection =
-      Firestore.instance.collection('lectures');
-
   final CollectionReference messagesCollection =
       Firestore.instance.collection('messages');
 
-  // int uid = 0;
-
-  // _getUID() async {
-  //   SharedPreferences prefs = await SharedPreferences.getInstance();
-  //   uid = (prefs.getInt('uid') ?? 0);
-  //   print('Firestore UID: $uid');
-  // }
-
   DatabaseService() {
     // _getUID();
-  }
-
-  Future updateData(
-      int uid,
-      String host_university,
-      String host_department,
-      String host_major,
-      String host_lecture_id,
-      String host_lecture_name,
-      String eras_university,
-      String eras_department,
-      String eras_major,
-      String eras_lecture_id,
-      String eras_lecture_name) async {
-    return await lectureCollection
-        .document()
-        .collection("$uid")
-        .document()
-        .setData({
-      'host_university': host_university,
-      'host_department': host_department,
-      'host_major': host_major,
-      'host_lecture_id': host_lecture_id,
-      'host_lecture_name': host_lecture_name,
-      'eras_university': eras_university,
-      'eras_department': eras_department,
-      'eras_major': eras_major,
-      'eras_lecture_id': eras_lecture_id,
-      'eras_lecture_name': eras_lecture_name,
-    });
   }
 
   Future sendContactForm(
